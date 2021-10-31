d0e9aea437c6243487f42394f4b0d54ecd8dae25
Kacper Walendzik <walendzik.kacper@outlook.com>
2021-10-31 23:17:43 +0100

    add FixLite + MongoDB

---
+++ Application/Dto/FixLiteDto.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Models.MongoDB;
+using System;
+
+namespace WebAPI.DtoModels
+{
+    public class FixLiteDto
+    {
+        public string Id { get; set; }
+        public int? Shipment { get; set; }
+        public string Date { get; set; }
+        public string CompanyName { get; set; }
+    }
+
+    public static class FixLiteExtension
+    {
+        public static FixLiteDto ConvertToDto(this FixLite model)
+        {
+            return new FixLiteDto
+            {
+                Id = model.Id,
+                Shipment = model.Shipment,
+                Date = model.Date,
+                CompanyName = model.CompanyName,
+            };
+        }
+    }
+}
---
+++ Domain/Models/MongoDB/FixLite.cs
@@ -0,0 +1,61 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace Domain.Models.MongoDB
+{
+    public class FixLite
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public int? Shipment { get; set; }
+        public string Date { get; set; }
+        public string CompanyName { get; set; }
+        public decimal? _1Element_PT { get; set; }
+        public decimal? _1Element_PD { get; set; }
+        public decimal? _1Element_RH { get; set; }
+        public decimal? _2Element_PT { get; set; }
+        public decimal? _2Element_PD { get; set; }
+        public decimal? _2Element_RH { get; set; }
+        public decimal? _3Element_PT { get; set; }
+        public decimal? _3Element_PD { get; set; }
+        public decimal? _3Element_RH { get; set; }
+
+        public decimal? _PT_Price { get; set; }
+        public decimal? _PT_Result { get; set; }
+        public decimal? _PT_Percent { get; set; }
+        public decimal? _PT_Weight { get; set; }
+        public decimal? _PT_Sum { get; set; }
+        public decimal? _PD_Price { get; set; }
+        public decimal? _PD_Result { get; set; }
+        public decimal? _PD_Percent { get; set; }
+        public decimal? _PD_Weight { get; set; }
+        public decimal? _PD_Sum { get; set; }
+        public decimal? _RH_Price { get; set; }
+        public decimal? _RH_Result { get; set; }
+        public decimal? _RH_Percent { get; set; }
+        public decimal? _RH_Weight { get; set; }
+        public decimal? _RH_Sum { get; set; }
+        public decimal? _PT_RD_RH_Sum_Sum { get; set; }
+        public decimal? _Cost_Weight { get; set; }
+        public decimal? _Cost_Price { get; set; }
+        public decimal? _Cost_Sum { get; set; }
+        public decimal? _All_Sum { get; set; }
+        public decimal? _All_Cost { get; set; }
+        public decimal? _All_Percent { get; set; }
+        public decimal? _All_All_Result { get; set; }
+        public decimal? _PW_Sum { get; set; }
+        public decimal? _PW_Wieght { get; set; }
+        public decimal? _PW_Price_Weight { get; set; }
+        public decimal? _Invoice_Wegiht { get; set; }
+        public decimal? _Invoice_Price { get; set; }
+        public decimal? _Invoice_Sum { get; set; }
+        public decimal? _Currency_Value { get; set; }
+        public decimal? _Currency_InvoiceSum { get; set; }
+        public decimal? _CP_Sum { get; set; }
+        public decimal? _CP_Weight { get; set; }
+        public decimal? _CP_Price_Weight { get; set; }
+    }
+}
